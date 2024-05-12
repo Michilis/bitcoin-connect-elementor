b1ffee18e1f1443ddc51f7b24e8884271d7a330d
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-12 02:51:00 +0200

    Create class-bc-login-handler.php

---
+++ includes/class-bc-login-handler.php
@@ -0,0 +1,7 @@
+<?php
+class BC_Login_Handler {
+    public static function handle_wallet_login($wallet_address) {
+        // Add login or user creation logic here
+        // For simplicity, this is just a placeholder
+    }
+}
